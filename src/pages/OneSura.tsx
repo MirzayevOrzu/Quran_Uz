eb747d197cab04c878354eef6c0d712aadb6fe42
Mirzayev Orzu <mirzayevorzu127@gmail.com>
2021-06-20 01:43:54 +0500

    optimized audioEl.onpause event

--- src/pages/OneSura.tsx
+++ src/pages/OneSura.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import ReactAudioPlayer from 'react-audio-player';
-import axios from 'axios'
+import axios from 'axios';
 
 interface Params {
     id: string
@@ -11,12 +11,20 @@ function OneSura() {
     const [suraDataText, setSuraDataText] = React.useState<any>({});
     const [suraDataAudio, setSuraDataAudio] = React.useState<any>({});
     const [state, setState] = React.useState<boolean>(false);
-    const [pointer, setPointer] = React.useState<number>(0);
     let params: Params = useParams();
 
     React.useEffect(() => {
         axios.get(`https://api.alquran.cloud/v1/surah/${params.id}/en.asad`)
         .then(res => {
+            axios.get(`http://api.alquran.cloud/v1/surah/${params.id}/ar.alafasy`)
+            .then(res => {
+                console.log(res.data.data);
+                setSuraDataAudio(res.data.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
             console.log(res.data.data);
             setSuraDataText(res.data.data);
             setState(true);
@@ -25,33 +33,32 @@ function OneSura() {
             console.log(err);
         })
 
-        axios.get(`http://api.alquran.cloud/v1/surah/${params.id}/ar.alafasy`)
-        .then(res => {
-            console.log(res.data.data);
-            setSuraDataAudio(res.data.data);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-
     }, []);
 
     const handlePlay = () => {
+        var pointer = 0;
         var audioCount = suraDataAudio.ayahs.length;
-        var audiosToPlay = document.getElementById("sura-auto-play") as HTMLAudioElement;
+        var audioElement = document.getElementById("sura-auto-play") as HTMLAudioElement;
         
         function onload(){
-            audiosToPlay.addEventListener('ended',onaudioEnded,false);
-            let source = suraDataAudio.ayahs[pointer]
-            audiosToPlay.src = source.audio;
-            audiosToPlay.play();
+            audioElement.addEventListener('pause', onaudioPaused, true);
+            audioElement.addEventListener('ended',onaudioEnded,false);
+            audioElement.src = suraDataAudio.ayahs[pointer].audio;
+            audioElement.play();
         }
         
-        function onaudioEnded(){    
-            setPointer(prev => prev + 1);
-            if (pointer > audioCount-1) return
-            // audiosToPlay.src = suraDataAudio[audio_index];
-            // videosToPlay.play();
+        function onaudioPaused(e: any) {
+            // console.log('paused');
+            // audioElement.removeEventListener('ended', onaudioEnded);
+        }
+
+        function onaudioEnded(){  
+            // console.log('ended')
+            pointer += 1;
+            if (pointer > audioCount-1) {
+                pointer = 0;
+                return
+            }
             onload();
         }
         
@@ -63,38 +70,15 @@ function OneSura() {
             <h1 className='arabic'>Suratul {suraDataText.name ? suraDataText.name : 'no info'}</h1>
             <button onClick={handlePlay}>Start</button>
 
-            <ReactAudioPlayer
-                id='sura-auto-play'
-                controls
-            />
+            <ReactAudioPlayer id='sura-auto-play' controls />
 
-            {/* { audioState && suraDataAudio.ayahs.map((ayah: {
-                number: number,
-                audio: string,
-                audioSecondary: string[],
-                hizbQuarter: number,
-                juz: number,
-                manzil: number,
-                numberInSurah: number,
-                page: number,
-                ruku: number,
-                sajda: boolean,
-                text: string,
-            }) => {
-                return <ReactAudioPlayer
-                src={ayah.audio!}
-                autoPlay
-                controls
-              />
-            })} */}
-
-            { state && suraDataText.ayahs.map((ayah: {number: number, text: string}, index: number) => (
-                <div  key={ayah.number}>
-                   <p className="arabic">
-                    {ayah.number} - {ayah.text} - {suraDataAudio.ayahs[index].text}
-                   </p>
-                </div>
-            ))}
+            { state && suraDataText.ayahs.map((ayahTranslation: {number: number, text: string}, i: number) => {
+                    return <div key={new Date().toLocaleDateString() + i} >
+                        <p className="arabic">
+                            {ayahTranslation.text} 
+                        </p>
+                    </div>
+            })}
         </div>
     )
 }
